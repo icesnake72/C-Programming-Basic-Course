8d51add8fe13093ced56f4d3a81d24e550deff7c
eunbum <icesnake72@gmail.com>
2023-03-14 16:52:18 +0900

    two-dims-example-start

--- ptrEx01.c
+++ ptrEx01.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  int a = 1;
-  printf("%d", a);
+  
+  short nums[2][3] = {{8,9,10},{11,12,13}};
+  for(int i=0; i<2; i++)
+  {
+    for(int j=0; j<3; j++) 
+      printf("%d, ", nums[i][j]);
+    
+    printf("\n");
+  }
+    
 
   return 0;
 }
