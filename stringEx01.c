80db0e60cdfa15fe78a84a3ab06fbcda5ec24501
eunbum <icesnake72@gmail.com>
2023-03-15 00:09:13 +0900

    Initializing and allocation test for string

--- stringEx01.c
+++ stringEx01.c
@@ -8,7 +8,14 @@ int main()
 
   printf("%s\n", str1);
   printf("%s\n", str2);
-  printf("%p\n", str2);
+
+  str1[0] = 'C';            // 가능
+  printf("%s\n", str1);
+
+  // str2[0] = 'T';         // 불가능
+  // printf("%s\n", str2);
+
+  // printf("%p\n", str2);
   
   str2 = "I am a software developer";
   printf("%s\n", str2);  
